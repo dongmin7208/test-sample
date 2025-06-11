87af7d582832c6d81945eb8ebb2fead9289ce521
dongmin7208 <sayg3o3d@gmail.com>
2025-06-11 12:45:45 +0900

    ADD: front docker init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3000,
-    proxy: { "/api": "http://localhost:3001" },
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
   },
 });
